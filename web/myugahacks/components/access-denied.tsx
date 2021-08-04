a816655bc61ecca977c82be5c6507246b50b6d84
Neal Patel <neal@patel.codes>
2021-08-04 17:30:27 -0400

    [release] bootstrapped myugahacks with next-auth

---
+++ web/myugahacks/components/access-denied.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/Link'
+import { signIn } from 'next-auth/client'
+
+export default function AccessDenied () {
+  return (
+    <>
+      <h1>Access Denied</h1>
+      <p>
+        <Link href="/api/auth/signin"
+           onClick={(e) => {
+           e.preventDefault()
+           signIn()
+        }}>You must be signed in to view this page</Link>
+      </p>
+    </>
+  )
+}
