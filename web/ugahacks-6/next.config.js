3e4fcdb3f4a0d387900637731698e5488e56e629
Bryan <bryansmith1117@gmail.com>
2021-08-25 14:42:03 -0400

    second attempt at fixing indentation

--- web/ugahacks-6/next.config.js
+++ web/ugahacks-6/next.config.js
@@ -3,9 +3,9 @@ module.exports = {
   async redirects(){
     return[
       {
- 	source: '/recap',
-	destination: 'https://storage.googleapis.com/ugahacks-public/external/UGAHacks6-Recap-Packet.pdf',
-	permanent: true,
+      	source: '/recap',
+	      destination: 'https://storage.googleapis.com/ugahacks-public/external/UGAHacks6-Recap-Packet.pdf',
+	      permanent: true,
       },
     ]
   },
