4b3c4d759a570064afc4292209ecb768feba093e
Andrei Spatariu <andros415@gmail.com>
2021-08-30 08:32:33 -0400

    Fixed global styling issue, imported fonts and favicons

--- web/ugahacks-6/pages/index.tsx
+++ web/ugahacks-6/pages/index.tsx
@@ -1,27 +1,33 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Splash from './components/Splash/Splash'
-import HallOfFame from './components/HallOfFame/HallOfFame';
-import About from './components/About/About';
-import Schedule from './components/Schedule/Schedule';
-import FAQ from './components/FAQ/FAQ';
-import Sponsors from './components/Sponsors/Sponsors';
-import Footer from './components/Footer/Footer';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Splash from "./components/Splash";
+import HallOfFame from "./components/HallOfFame";
+import About from "./components/About";
+import Schedule from "./components/Schedule";
+import FAQ from "./components/FAQ";
+import Sponsors from "./components/Sponsors";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    
     <div className={styles.container}>
-              <Splash />
-              <HallOfFame />
-              <About />
-              <Schedule />
-              <FAQ />
-              <Sponsors />
-              <Footer />
+      <Head>
+        <title>UGAHacks 6</title>
+        <meta
+          name="description"
+          content="UGAHacks 6 Event Site - Boot up your dreams and build the future! February 5-7, 2021"
+        />
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
 
+      <Splash />
+      {/* <HallOfFame /> */}
+      <About />
+      <Schedule />
+      <FAQ />
+      <Sponsors />
+      <Footer />
     </div>
-  )
+  );
 }
-
