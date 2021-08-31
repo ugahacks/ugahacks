2e6be7f528ba9747a9f11abb6b6ae83d77cbc482
Genki Aikawa <ga07600@uga.edu>
2021-08-31 12:25:47 -0400

    Resolved merge conflict by adding both changes

--- web/ugahacks-6/next.config.js
+++ web/ugahacks-6/next.config.js
@@ -1,13 +1,23 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    HOSTNAME: "localhost"
+    HOSTNAME: "localhost",
   },
   settings: {
-    'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
-  }
-}
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  async redirects() {
+    return [
+      {
+        source: "/recap",
+        destination:
+          "https://storage.googleapis.com/ugahacks-public/external/UGAHacks6-Recap-Packet.pdf",
+        permanent: true,
+      },
+    ];
+  },
+};
