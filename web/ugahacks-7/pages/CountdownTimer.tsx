5f93616b99267ae8a1644298b85750e6e4040ad5
Calla Corder <cnc45333@uga.edu>
2021-10-01 15:08:36 -0400

    updated background and started the countdown component

---
+++ web/ugahacks-7/pages/CountdownTimer.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const CountDownTimer = ({hoursMinSecs}: {hoursMinSecs:any}) => {
+   
+    const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+    const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
+    
+
+    const tick = () => {
+   
+        if (hrs === 0 && mins === 0 && secs === 0) 
+            setTime([0,0,0]);
+        else if (mins === 0 && secs === 0) {
+            setTime([hrs - 1, 59, 59]);
+        } else if (secs === 0) {
+            setTime([hrs, mins - 1, 59]);
+        } else {
+            setTime([hrs, mins, secs - 1]);
+        }
+    };
+
+
+    const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+    
+    React.useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
+    
+    return (
+        <div>
+            <p>{`${hrs.toString().padStart(2, '0')}:${mins
+            .toString()
+            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p> 
+        </div>
+    );
+}
+
+export default CountDownTimer;
--- web/ugahacks-7/pages/splash.tsx
+++ web/ugahacks-7/pages/splash.tsx
@@ -1,15 +1,19 @@
-//import '../styles/globals.css'
 import styles from '../styles/splash.module.css'
+import CountDownTimer from './CountdownTimer';
 
 export function Splash() {
+
+const hoursMinSecs={hours:0, minutes:0, seconds:10}
+
     return (
-        <div>
-        <div className={styles.greyTop}></div>
-        <div className={styles.border}>
-            <div className={styles.main}>
-                <a href="https:my.ugahacks.com/apply"><button className={styles.redirectBtn}></button></a>
+        <div className={styles.body}>
+            <div className={styles.greyTop}></div>
+            <div className={styles.border}>
+                <div className={styles.main}>
+                    <a href="https:my.ugahacks.com/apply"><button className={styles.redirectBtn}></button></a>
+                    <CountDownTimer hoursMinSecs={hoursMinSecs}/>
+                </div>
             </div>
-        </div>
         </div>
     )
 
