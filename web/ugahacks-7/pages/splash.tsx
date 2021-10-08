69467b457d5fdbfbbafe9146cdb989328127ccc7
Calla Corder <cnc45333@uga.edu>
2021-10-08 13:05:10 -0400

    Splash page complete with countdown at top and button over most of page that brings users to dummy site for now

--- web/ugahacks-7/pages/splash.tsx
+++ web/ugahacks-7/pages/splash.tsx
@@ -1,17 +1,13 @@
+import Countdown from './countdown'
 import styles from '../styles/splash.module.css'
-import CountDownTimer from './CountdownTimer';
 
 export function Splash() {
-
-const hoursMinSecs={hours:0, minutes:0, seconds:10}
-
     return (
         <div className={styles.body}>
-            <div className={styles.greyTop}></div>
+            <div className={styles.greyTop}><Countdown/></div>
             <div className={styles.border}>
                 <div className={styles.main}>
                     <a href="https:my.ugahacks.com/apply"><button className={styles.redirectBtn}></button></a>
-                    <CountDownTimer hoursMinSecs={hoursMinSecs}/>
                 </div>
             </div>
         </div>
