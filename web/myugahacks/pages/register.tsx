39d22f9f9bc767822ec8c29b9b767e7ddd28e995
Genki Aikawa <ga07600@uga.edu>
2021-10-19 11:33:53 -0400

    UGA Hacks 7 Form Page

--- web/myugahacks/pages/register.tsx
+++ web/myugahacks/pages/register.tsx
@@ -1,7 +1,36 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
+
 import styles from "./register.module.css";
 
 export default function Register(): ReactElement {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    university: "",
+    degree: "",
+    github: "",
+    linkedin: "",
+    website: "",
+    ethnicity: "",
+    firstHack: "",
+    firstUGAHack: "",
+    reimburse: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    return (e: React.MouseEvent) => {
+      e.preventDefault();
+    };
+  };
+
   return (
     <React.Fragment>
       <div className={styles.register}>
@@ -9,49 +38,90 @@ export default function Register(): ReactElement {
         <h2>Personal Information</h2>
         <form>
           <label>First Name</label>
-          <input type="text" required></input>
+          <input
+            value={values.firstName}
+            type="text"
+            required
+            name="firstName"
+            onChange={handleChange}
+          ></input>
           <label>Last Name</label>
-          <input type="text" required></input>
+          <input
+            value={values.lastName}
+            type="text"
+            required
+            name="lastName"
+            onChange={handleChange}
+          ></input>
           <label>Phone Number</label>
-          <input type="text" required></input>
+          <input
+            value={values.phoneNumber}
+            type="text"
+            required
+            name="phoneNumber"
+            onChange={handleChange}
+          ></input>
           <label>University</label>
-          <input type="text" required></input>
+          <input
+            value={values.university}
+            type="text"
+            required
+            name="university"
+            onChange={handleChange}
+          ></input>
           <label>Degree</label>
-          <input type="text" required></input>
-        </form>
-      </div>
-      <div className={styles.register}>
-        <h2>UGA Hacks Information</h2>
-        <form>
+          <input
+            value={values.degree}
+            type="text"
+            required
+            name="degree"
+            onChange={handleChange}
+          ></input>
+
+          <h2>UGA Hacks Information</h2>
+
           <label>Is this your first hackathon?</label>
-          <select>
+          <select value={values.firstHack} name="firstHack" onChange={handleChange}>
             <option value="Yes">Yes!</option>
             <option value="No">No!</option>
           </select>
           <label>Have you attended previous UGA Hacks?</label>
-          <select>
+          <select value={values.firstUGAHack} name="firstUGAHack" onChange={handleChange}>
             <option value="Yes">Yes!</option>
             <option value="No">No</option>
           </select>
           <label>Would you like to apply for travel reimbursement?</label>
-          <select>
+          <select value={values.reimburse} name="reimburse" onChange={handleChange}>
             <option value="Yes">Yes</option>
             <option value="No">No</option>
           </select>
-        </form>
-      </div>
-      <div className={styles.register}>
-        <h2>Show us what you have built!</h2>
-        <form>
+
+          <h2>Show us what you have built!</h2>
+
           <label>Gitub (It is ok if you do not have one!)</label>
-          <input type="text"></input>
+          <input
+            value={values.github}
+            type="text"
+            name="github"
+            onChange={handleChange}
+          ></input>
           <label>Linkedin (It is ok if you do not have one!)</label>
-          <input type="text"></input>
+          <input
+            value={values.linkedin}
+            type="text"
+            name="linkedin"
+            onChange={handleChange}
+          ></input>
           <label>Personal Website (It is ok if you do not have one!)</label>
-          <input type="text"></input>
+          <input
+            value={values.website}
+            type="text"
+            name="website"
+            onChange={handleChange}
+          ></input>
           <h2>Private Information</h2>
           <label>Ethnicity?</label>
-          <select>
+          <select value={values.ethnicity} name="ethnicity" onChange={handleChange}>
             <option value="amIndian">American Indian or Alaskan Native</option>
             <option value="asian">Asian/Pacific Islander</option>
             <option value="aAm">Black or African American</option>
@@ -60,6 +130,7 @@ export default function Register(): ReactElement {
             <option value="multiple">Multiple ethnicities/Other</option>
             <option value="noAnswer">Prefer not to answer</option>
           </select>
+          <button onClick={handleSubmit}>Submit</button>
         </form>
       </div>
     </React.Fragment>
