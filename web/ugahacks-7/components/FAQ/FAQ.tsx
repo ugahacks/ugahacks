c922ce7bf19dbd92777c472c08677b79b92ea1f4
Genki Aikawa <ga07600@uga.edu>
2022-01-31 11:16:00 -0500

    Added the chat box that contains the faqs and answers

--- web/ugahacks-7/components/FAQ/FAQ.tsx
+++ web/ugahacks-7/components/FAQ/FAQ.tsx
@@ -4,6 +4,14 @@ import styles from "../../styles/FAQ.module.css";
 
 export { faqQuestions };
 
+const questions: string[] = [
+  "Where will it be held this year?",
+];
+
+const answers: string[] = [
+  "The event will be a hybrid model where it will be located in MLC for in-person and virtual over slack!",
+];
+
 const FAQ = (): ReactElement => {
     const faq = "<h1>Hello</h1>"
     return(
@@ -19,15 +27,52 @@ const FAQ = (): ReactElement => {
     );
 };
 
+function faq(): ReactElement {
+  return(
+    <>
+      <span className={styles.faq}>FAQ: </span>
+    </>
+  )
+};
+
+function byte(): ReactElement {
+  return(
+    <>
+      <span className={styles.byte}>Byte: </span>
+    </>
+  )
+};
+
+function question(index: number): ReactElement {
+  return(
+    <>
+      <li className={styles.questions}>
+        {faq()}
+        {questions[index]}
+      </li>
+    </>
+  )
+};
+
+function answer(index: number): ReactElement {
+  return(
+    <>
+      <li className={styles.answers}>
+        {byte()}
+        {answers[index]}
+      </li>
+    </>
+  )
+};
+
 function faqQuestions(): ReactElement {
     return (
       <>
-        <div className="chat">
-          <div className="field-row-stacked">
-            <textarea className={styles.chatBody}>
-
-            </textarea>
-          </div>
+        <div className={styles.faqContainer}>
+          <ul className="tree-view">
+            {question(0)}
+            {answer(0)}
+          </ul>
         </div>
       </>
     );
