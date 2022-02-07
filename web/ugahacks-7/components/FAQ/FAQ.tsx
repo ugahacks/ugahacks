a16c619be7609e2443c0c40a813a62f22612401a
Genki Aikawa <ga07600@uga.edu>
2022-02-07 18:43:59 -0500

    Removed unnecessary code and added comments

--- web/ugahacks-7/components/FAQ/FAQ.tsx
+++ web/ugahacks-7/components/FAQ/FAQ.tsx
@@ -1,11 +1,13 @@
-import React, { ReactElement, useState, useEffect } from "react";
+import React, { ReactElement, useState } from "react";
 import Window from "../Window";
 import styles from "../../styles/FAQ.module.css";
 
 export { faqQuestions };
 
+// Create an array of topics that will be accessed by the option HTML tag
 const topics: string[] = ["COVID", "Logistics", "About the Hackathon"];
 
+// Question and answer arrays for each topic
 const questions_COVID: string[] = [
   "Where will it be held this year?",
   "How much will it cost to sign up?",
@@ -26,20 +28,35 @@ const answers_logistics: string[] = [
   "Logistics Answers"
 ];
 
+const questions_hackathon: string[] = [
+  "Questions about the Hackathon"
+];
+
+const answers_hackathon: string[] = [
+  "Answers about the Hackathon"
+];
+
+/**
+ * Uses the useState hook to check which FAQs to display based on the topic. We
+ * are setting the topic based on the option values.
+ *
+ * @returns {ReactElement} The dropdown menu and FAQ textarea will be rendered everytime
+ * a new topic is selected.
+ */
 function CurrentTopic(): ReactElement {
   const [topic, setTopic] = useState("COVID");
 
   const inputHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setTopic(e.target.value)
-  }
+    setTopic(e.target.value);
+  };
 
   return (
     <>
-      <label>Topics: </label>
+      <label className={styles.topic}>Topics: </label>
       <select value={topic} onChange={inputHandler} className={styles.dropdown}>
         <option value={"COVID"}>{topics[0]}</option>
         <option value={"Logistics"}>{topics[1]}</option>
-        <option value={"2"}>{topics[2]}</option>
+        <option value={"About the Hackathon"}>{topics[2]}</option>
       </select>
       {generateFAQ(topic)}
     </>
@@ -66,47 +83,29 @@ const FAQ = (): ReactElement => {
 };
 
 /**
- * Combine the questions and answers array and display it in the chatbox.
- * We are combining the questions and answers array in an alternating format so
- * that FAQ and Byte will be alternating.
+ * Based on the topic selected from the dropdown menu, the FAQ textarea will change its
+ * window by selecting the correct case in the switch case clause. Each case will be a
+ * topic for the FAQ. More topics can be added to the topics array and CurrentTopic()
+ * functional component.
  *
  * @return {ReactElement} Returns a <ul> containing <li> of questions and answers.
  */
 function generateFAQ(type: string): ReactElement {
   let faq: ReactElement[] = [];
 
-  switch(type) {
+  switch (type) {
     case "COVID":
-      for (var i = 0; i < questions_COVID.length; i++) {
-        faq.push(
-          <li className={styles.questions}>
-            <span className={styles.faq}>FAQ: </span>
-            {questions_COVID[i]}
-          </li>
-        );
-        faq.push(
-          <li className={styles.answers}>
-            <span className={styles.byte}>Byte: </span>
-            {answers_COVID[i]}
-          </li>
-        );
-      } // for
+      faq = generateQAArray(questions_COVID, answers_COVID);
       break;
-    case "Logistics": 
-      for (var i = 0; i < questions_logistics.length; i++) {
-        faq.push(
-          <li className={styles.questions}>
-            <span className={styles.faq}>FAQ: </span>
-            {questions_logistics[i]}
-          </li>
-        );
-        faq.push(
-          <li className={styles.answers}>
-            <span className={styles.byte}>Byte: </span>
-            {answers_logistics[i]}
-          </li>
-        );
-      } // for
+    case "Logistics":
+      faq = generateQAArray(questions_logistics, answers_logistics);
+      break;
+    case "About the Hackathon":
+      faq = generateQAArray(questions_hackathon, answers_hackathon);
+      break;
+    default:
+      faq = generateQAArray(questions_COVID, answers_logistics);
+      break;
   }
 
   return (
@@ -116,23 +115,45 @@ function generateFAQ(type: string): ReactElement {
   );
 }
 
-function topicDropdown(): ReactElement {
-  return (
-    <>
-    <label>Topics: </label>
-      <select className={styles.dropdown}>
-        <option value={0}>{topics[0]}</option>
-        <option value={1}>{topics[1]}</option>
-        <option value={2}>{topics[2]}</option>
-      </select>
-    </>
-  )
-}
+/**
+ * Generates alternating questions and answers array so that the questions and answers will
+ * display in an alternating fashion in the FAQ textarea. This is a helper function to the
+ * generateFAQ() function. Returns an array of alternating quesitons and answers so that the
+ * generateFAQ() function can iterate through the returned array and display it in the textarea.
+ *
+ * @param question_array the array containing the questions
+ * @param answer_array the array containing the answers
+ *
+ * @returns {ReactElement} Returns the list of FAQ (questions) and Byte (answers) in JSX.
+ */
+function generateQAArray(
+  question_array: string[],
+  answer_array: string[]
+): ReactElement[] {
+  let faq: ReactElement[] = [];
+
+  for (let i = 0; i < question_array.length; i++) {
+    faq.push(
+      <li className={styles.questions}>
+        <span className={styles.faq}>FAQ: </span>
+        {question_array[i]}
+      </li>
+    );
+    faq.push(
+      <li className={styles.answers}>
+        <span className={styles.byte}>Byte: </span>
+        {answer_array[i]}
+      </li>
+    );
+  } // for
+
+  return faq;
+} // generateQA
 
 /**
- * This function calls the generateFAQ() function to display all the faqs 
+ * This function calls the generateFAQ() function to display all the faqs
  * in the chatbox. This function is being exported to Window.tsx.
- * 
+ *
  * @returns {ReactElement} Returns the faq container chatbox.
  */
 function faqQuestions(): ReactElement {
@@ -140,13 +161,7 @@ function faqQuestions(): ReactElement {
 
   return (
     <>
-      <div className={styles.faqContainer}>
-        {/* <ul className="tree-view">
-          {question(0)}
-          {answer(0)}
-        </ul> */}
-        {CurrentTopic()}
-      </div>
+      <div className={styles.faqContainer}>{CurrentTopic()}</div>
     </>
   );
 }
