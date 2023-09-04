b36becfafde7b03b780a0e5e93ea218b9b4a8db9
Shawn Pradeep <shawnprad@hotmail.com>
2023-09-04 14:23:53 -0400

    included openssl-legacy-provider scripts for uh6-8 & make8

---
+++ scripts/lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the NODE_OPTIONS environment variable
+NODE_OPTIONS=--openssl-legacy-provider
+yarn next lint
+
+# Display a message to confirm that the variable is set
+echo "NODE_OPTIONS is set to: $NODE_OPTIONS"
