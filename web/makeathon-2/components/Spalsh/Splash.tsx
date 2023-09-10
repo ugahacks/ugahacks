809a176395a297991d2177aeef792a03171237a4
Shawn Pradeep <shawnprad@hotmail.com>
2023-09-10 13:57:07 -0400

    Added ocean surface wave transition betwen Splash -> About + About -> FAQ

--- web/makeathon-2/components/Spalsh/Splash.tsx
+++ web/makeathon-2/components/Spalsh/Splash.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../styles/Splash.module.css";
 
 function Splash() {
-  return <section className={styles.body}>Splash</section>;
+  return (
+    <section className={styles.body}>
+      Splash
+      {/* <Image
+        className={styles.ocean_surface}
+        src="/graphics/ocean-splash-transition.svg"
+        width={1500}
+        height={500}
+        alt="ocean surface"
+      /> */}
+    </section>
+  );
 }
 
 export default Splash;
