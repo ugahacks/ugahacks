2c6058aa33747b8d404481f11066692af4a4272d
Shawn Pradeep <shawnprad@hotmail.com>
2023-09-11 21:26:22 -0400

    Included rel=noreferer to ensure compatability on older browsers

--- web/makeathon-2/components/Footer/Footer.tsx
+++ web/makeathon-2/components/Footer/Footer.tsx
@@ -7,27 +7,43 @@ function Footer() {
       <div className={styles.container}>
         <div className={styles.icon_row}>
           <div className={styles.icon}>
-            <a href="https://www.instagram.com/ugahacks/" target="_blank">
+            <a
+              href="https://www.instagram.com/ugahacks/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/icons/instagram_icon.svg" alt="instagram" />
             </a>
           </div>
           <div className={styles.icon}>
-            <a href="https://www.facebook.com/ugahacks/" target="_blank">
+            <a
+              href="https://www.facebook.com/ugahacks/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/icons/facebook_icon.svg" alt="facebook" />
             </a>
           </div>
           <div className={styles.icon}>
-            <a href="https://www.linkedin.com/company/ugahacks" target="_blank">
+            <a
+              href="https://www.linkedin.com/company/ugahacks"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/icons/linkedin_icon.svg" alt="linkedin" />
             </a>
           </div>
           <div className={styles.icon}>
-            <a href="https://www.github.com/ugahacks/" target="_blank">
+            <a
+              href="https://www.github.com/ugahacks/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/icons/github_icon.svg" alt="github" />
             </a>
           </div>
           <div className={styles.icon}>
-            <a href="https://ugahacks.com/" target="_blank">
+            <a href="https://ugahacks.com/" target="_blank" rel="noreferrer">
               <img src="/icons/site_icon.svg" alt="site" />
             </a>
           </div>
