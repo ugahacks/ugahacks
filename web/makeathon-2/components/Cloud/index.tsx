d91bee9031f09db441de8ed9a8e81503ec125aeb
alexteal <29980694+alexteal@users.noreply.github.com>
2023-09-18 22:23:53 -0400

    Possible cloud generation schema

---
+++ web/makeathon-2/components/Cloud/index.tsx
@@ -0,0 +1,3 @@
+import Cloud from "./Cloud";
+
+export default Cloud;
--- web/makeathon-2/components/Spalsh/Splash.tsx
+++ web/makeathon-2/components/Spalsh/Splash.tsx
@@ -1,33 +1,12 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Splash.module.css";
-
+import Cloud from "../Cloud";
 function Splash() {
-  return (
-    <section className={styles.body}>
-      Splash
-      {/* <div className={styles.container}>
-        <div className={styles.row}>
-          <img
-            className={styles.logo}
-            src="/graphics/Makeathon Transparent Logo resize.png"
-            alt="logo"
-          />
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tagline}>tagline</div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.subtitle}>Driftmier Engineering Center</div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.register}>
-            <button className="button">Click Me</button>
-          </div>
-        </div>
-      </div> */}
-    </section>
-  );
+    const numClouds = Math.floor(Math.random() * 4) + 1; // Random number between 1 and 4
+    const clouds = Array.from({ length: numClouds }, (_, i) => (
+        <Cloud key={i} />
+    ));
+    return <section className={styles.body}>{clouds}</section>;
 }
-
 export default Splash;
