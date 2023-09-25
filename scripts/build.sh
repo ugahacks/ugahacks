a8d38f7b35e2485f8e7aabbb8e4510c02baee182
Shawn Pradeep <93854780+shawnpradeep@users.noreply.github.com>
2023-09-24 21:57:49 -0400

    Revert "Makeathon II Production Push #1"

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the NODE_OPTIONS environment variable
-NODE_OPTIONS=--openssl-legacy-provider
-yarn next build
-
-# Display a message to confirm that the variable is set
-echo -e "\nNODE_OPTIONS was set to: $NODE_OPTIONS"
