a6f2691db98ca8698977d95ed24fd7c410e10a61
Genki Aikawa <ga07600@uga.edu>
2023-09-25 00:48:47 -0400

    activate cicd

--- web/makeathon-2/components/About/About.tsx
+++ web/makeathon-2/components/About/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../styles/About.module.css";
-// Cool stuff!
+// Cool stuff! yup
 function About() {
   return (
     <section className={styles.body}>
