7f1baa0f7d2157f5b2d283eeeaee67152bcf6d0b
Nikita Jha <nikitajha1912@gmail.com>
2023-10-16 23:48:57 -0400

    Adding splash page main image

--- web/ugahacks-9/app/page.tsx
+++ web/ugahacks-9/app/page.tsx
@@ -1,11 +1,15 @@
 // import Hello from "@/components/Hello";
 
 import Navbar from "@/components/Navbar/Navbar";
+import Splash from "./splash/Splash";
 
 export default function Home() {
   return (
     <div className="main">
+      
       <Navbar />
+      <Splash />
+      
     </div>
   );
 }
