522682a94a501c9556ec1647fa193d16e390ad78
Nikita Jha <nikitajha1912@gmail.com>
2023-10-31 19:48:40 -0400

    Adding placeholder images and text

--- web/ugahacks-9/app/splash/Splash.tsx
+++ web/ugahacks-9/app/splash/Splash.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import bannerImage from '../../public/banner.png'
-import '../../styles/Splash.css'
-import Image from 'next/image'
+import React from 'react';
+import bannerImage from '../../public/banner.png';
+import placeholderImage from '../../public/placeholder.png';
+import '../../styles/Splash.css';
+import Image from 'next/image';
 
 function Splash() {
   return (
     <div className="splash-container">
+      <div className="banner-container">
         <Image src={bannerImage} alt='Splash Image' className='splash-image'/>
+      </div>
 
+      <div className="images-row">
+        {['ABOUT', 'FAQ', 'OUR TEAM', 'SPONSORS'].map((text, index) => (
+          <div key={index} className="image-container">
+            <Image src={placeholderImage} alt={`Image ${index + 1}`} className='placeholder-image' />
+            <div className="image-text">{text}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Splash
+export default Splash;
