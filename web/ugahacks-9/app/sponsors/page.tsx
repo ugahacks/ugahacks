03e87974723e03f5e89f4c3d64571fce2b1a9bd7
Hannah <Hannah2520@gmail.com>
2023-11-12 18:06:57 -0500

    created a sponsors page

--- web/ugahacks-9/app/sponsors/page.tsx
+++ web/ugahacks-9/app/sponsors/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
+import styles from "../../styles/Sponsors.module.css";
 
 function Sponsors() {
   return (
     <div className="main">
       <Navbar />
+      <div className={styles.mainContainer}>
+        <div className={styles.sponsorsContainer}>
+
+        </div>
+         
+        <div className={styles.buttonContainer}>
+          <p className={styles.buttonHeading}>This event is supported in part by the President's Venture Fund through the generous gifts of University of Georgia donors.</p>
+         <button className={styles.sponsorshipPacketButton}>Sponsorship Packet</button>
+        </div>
+      </div>
     </div>
   );
 }
