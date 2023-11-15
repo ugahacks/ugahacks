d8822d688904382bd357e2eab81c790d2b6d446e
Nikita Jha <nikitajha1912@gmail.com>
2023-11-15 00:08:14 -0500

    Added marketing images to replace placeholder images

--- web/ugahacks-9/app/splash/Splash.tsx
+++ web/ugahacks-9/app/splash/Splash.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import bannerImage from '../../public/banner.png';
-import placeholderImage from '../../public/placeholder.png';
+import img1 from '../../public/img1.png';
+import img2 from '../../public/img2.png'
+import img3 from '../../public/img3.png'
+import img4 from '../../public/img4.png'
+
 import '../../styles/Splash.css';
 import Image from 'next/image';
 
 function Splash() {
+  const images = [img1, img2, img3, img4];
+
   return (
     <div className="splash-container">
       <div className="banner-container">
-        <Image src={bannerImage} alt='Splash Image' className='splash-image'/>
       </div>
-
       <div className="images-row">
         {['ABOUT', 'FAQ', 'OUR TEAM', 'SPONSORS'].map((text, index) => (
           <div key={index} className="image-container">
-            <Image src={placeholderImage} alt={`Image ${index + 1}`} className='placeholder-image' />
+            <Image src={images[index]} alt={`Image ${index + 1}`} className='placeholder-image' />
             <div className="image-text">{text}</div>
           </div>
         ))}
