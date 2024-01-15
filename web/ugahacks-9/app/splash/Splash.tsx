9dbeb9ffe29712ce54b87e6034761dc39ca22a9d
Shawn Pradeep <shawnprad@hotmail.com>
2024-01-15 01:10:40 -0500

    finished splash page

--- web/ugahacks-9/app/splash/Splash.tsx
+++ web/ugahacks-9/app/splash/Splash.tsx
@@ -1,44 +1,125 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Splash.module.css";
+import Link from "next/link";
 
 function Splash() {
   return (
     <div>
+      {/** byte punching through banner: */}
       <div className={styles.banner}>
-        <Image
-          src="/splash_artwork.png"
-          alt="uh9 banner"
-          layout="responsive"
-          width={1920}
-          height={1080}
-        />
+        <div className={styles.imageContainer}>
+          <Image
+            src="/banner2.svg"
+            alt="uh9 banner"
+            layout="responsive"
+            width={700}
+            height={200}
+          />
+        </div>
       </div>
+
+      {/** comic nav: */}
       <div className={styles.comic_nav}>
-        <div className={styles.imageText}>ABOUT</div>
-        <div className={styles.imageText}>FAQ</div>
-        <div className={styles.imageText}>OUT TEAM</div>
-        <div className={styles.imageText}>SPONSORS</div>
+        <div className={styles.button_container}>
+          <Link href={"/about"} style={{ textDecoration: "none" }}>
+            <Image
+              className={styles.button}
+              src="/buttons/about.svg"
+              layout="responsive"
+              alt="about button"
+              width={125}
+              height={161.8}
+            />
+            <div className={styles.button_text}>ABOUT</div>
+          </Link>
+        </div>
+        <div className={styles.button_container}>
+          <Link href={"/faq"} style={{ textDecoration: "none" }}>
+            <Image
+              src="/buttons/faq.svg"
+              layout="responsive"
+              alt="faq button"
+              width={125}
+              height={161.8}
+            />
+            <div className={styles.button_text}>FAQ</div>
+          </Link>
+        </div>
+        <div className={styles.button_container}>
+          <Link
+            href={"https://ugahacks.com/#team"}
+            style={{ textDecoration: "none" }}
+          >
+            <Image
+              src="/buttons/our_team.svg"
+              layout="responsive"
+              alt="our team button"
+              width={125}
+              height={161.8}
+            />
+            <div className={styles.button_text}>OUR TEAM</div>
+          </Link>
+        </div>
+        <div className={styles.button_container}>
+          <Link href={"/sponsors"} style={{ textDecoration: "none" }}>
+            <Image
+              src="/buttons/sponsors.svg"
+              layout="responsive"
+              alt="sponsor button"
+              width={125}
+              height={161.8}
+            />
+            <div className={styles.button_text}>SPONSORS</div>
+          </Link>
+        </div>
       </div>
 
       {/** speech bubbles: */}
       <div className={styles.speech_bubble1}>
-        <div className={styles.caption2}>UGA Miller Learning Center Feb 9 - 11</div>
         <Image
-          src="/speech_bubble1.png"
+          src="/speech_bubble_1.png"
+          layout="responsive"
           alt="speech bubble"
-          width={355}
-          height={227}
+          width={280}
+          height={200}
         />
       </div>
       <div className={styles.speech_bubble2}>
-        <div className={styles.caption}>Be the hero of your own story.</div>
         <Image
-          src="/speech_bubble2.png"
+          src="/speech_bubble_2.png"
+          layout="responsive"
           alt="speech bubble"
-          width={197}
+          width={247}
           height={165}
         />
+      </div>
+      <div className={styles.exclamation}>
+        <Image
+          src="/exclamation.svg"
+          alt="exclamation point"
+          width={225}
+          height={225}
+        />
+      </div>
+
+      {/** register button: */}
+      <div className={styles.register_container}>
+        <div className={styles.register_button}>
+          <Link
+            href="https://mybyte.ugahacks.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/buttons/register_button.svg"
+              layout="responsive"
+              alt="registration button"
+              width={400}
+              height={159.22}
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
