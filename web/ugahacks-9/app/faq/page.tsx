a789f5d1b223c437301ab746a2cf07f31b505430
Vansh Arora <vansh94arora@gmail.com>
2024-01-19 15:06:11 -0500

    Set up and display left and right sections; left middle section (FAQ questions) empty for now

--- web/ugahacks-9/app/faq/page.tsx
+++ web/ugahacks-9/app/faq/page.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
-
+import styles from "../../styles/FAQ.module.css";
+import normal_image1_1 from "@/public/faq_normal_image1-1.png";
+import normal_image1_2 from "@/public/faq_normal_image1-2.png";
+import normal_image1_3 from "@/public/faq_normal_image1-3.png";
+import normal_image2_1 from "@/public/faq_normal_image2-1.png";
+import byte_image from "@/public/byte faq 1.png";
 function FAQ() {
   return (
     <div className="main">
       <Navbar />
+      <div className={styles.content}>
+        <div className={styles.left_images}>
+          <div className={styles.faq_title}><h1>FAQ</h1></div>
+          <img className={styles.normal_image1} src={normal_image1_1.src}></img>
+          <img className={styles.normal_image1} src={normal_image1_2.src}></img>
+          <img className={styles.normal_image1} src={normal_image1_3.src} ></img>
+        </div>
+        <div className={styles.questions}>
+        questions
+        </div>
+        <div className={styles.right_images}>
+          <img className={styles.normal_image2} src={normal_image2_1.src}></img>
+          <img className={styles.byte_image} src={byte_image.src} ></img>
+        </div>
+      </div>
     </div>
   );
 }
