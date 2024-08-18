f914634a4c79c0d6b58fcf60d0fd262067442080
Akil <akilmir8222003@gmail.com>
2024-08-18 00:30:35 -0400

    Landing page updates

--- web/ugahacks-x/app/splash/Splash.tsx
+++ web/ugahacks-x/app/splash/Splash.tsx
@@ -14,20 +14,20 @@ const Splash = () => {
         />
       </div>
       <div className={styles.rockstarText}>
-        <h2>UNLEASH YOUR INNER ROCKSTAR</h2>
+        <h2>Unleash your inner rockstar</h2>
       </div>
-      <div className={styles.locationAndDateText}>
+      {/* <div className={styles.locationAndDateText}>
         <p>02/07/25 - 02/09/25</p>
         <p>Zell B. Miller Learning Center Athens, GA</p>
-      </div>
-      <button style={{backgroundColor: "#f472b6", borderRadius: "5px", marginTop: "5px"}}>
-      <a href="https://mybyte.ugahacks.com/" target="_blank" rel="noopener noreferrer" className={styles.registerButton}>
-          REGISTER NOW
+      </div> */}
+      <button className={styles.registerButton}>
+      <a href="https://mybyte.ugahacks.com/" target="_blank" rel="noopener noreferrer" className={styles.registerLink}>
+          Register Now
         </a>
       </button>
-      <button style={{fontWeight: "bold",  backgroundColor: "#f472b6", padding: "5px", borderRadius: "5px", marginTop: "10px"}}>
+      {/* <button style={{fontWeight: "bold",  backgroundColor: "#f472b6", padding: "5px", borderRadius: "5px", marginTop: "10px"}}>
       <a target="blank" href="http://hackp.ac/coc">MLH Code of Conduct</a>
-      </button>
+      </button> */}
     </div>
   );
 };
