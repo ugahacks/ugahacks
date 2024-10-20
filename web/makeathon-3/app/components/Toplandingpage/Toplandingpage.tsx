f3f31d50e75994a029faf2dda3ea705a499eb3f2
dhruv2253 <dhruv225@outlook.com>
2024-10-20 17:23:18 -0400

    Add landingpage svg and background

---
+++ web/makeathon-3/app/components/Toplandingpage/Toplandingpage.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+export default function Toplandingpage() {
+  return (
+    <>
+        <Image 
+        src="/LandingPage.svg"
+        alt="Landing Page image"
+        width={1920}            
+        height={1080}
+        />
+    </>
+  );
+}
