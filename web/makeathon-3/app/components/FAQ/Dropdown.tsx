05af23cd7c43cd78a8a66f25ce9c8b30fad56e09
TheRyanMajd <rm97798@uga.edu>
2024-10-24 00:03:47 -0400

    Refactor FAQ Dropdown component styles now it looks nice!

--- web/makeathon-3/app/components/FAQ/Dropdown.tsx
+++ web/makeathon-3/app/components/FAQ/Dropdown.tsx
@@ -17,16 +17,18 @@ const Dropdown: React.FC<DropdownProps> = ({ title, content }) => {
     return (
         <div className="w-[667px] mt-4">
             <button
-                className="w-full h-[62.01px] bg-[rgba(51,103,41,0.18)] border-[4px] border-[#336729] 
-                rounded-[29.167px] text-left pl-4 pr-10 flex items-center justify-between cursor-pointer"
+                className="w-full h-[62px] bg-[rgba(51,103,41,0.18)] border-[4px] border-[#336729] 
+                rounded-[29.167px] text-left pl-4 pr-10 flex items-center font-bold justify-between cursor-pointer text-green-700"
                 onClick={toggleDropdown}
             >
                 {title}
                 <span className="ml-auto pr-4">{isOpen ? '▲' : '▼'}</span>
             </button>
+            <p className='mt-1'></p>
             {isOpen && (
-                <div className="bg-[rgba(51,103,41,0.18)] w-full border-t-[4px] border-x-[4px] border-[#336729] 
-                rounded-b-[29.167px] py-2 shadow-lg">
+                <div className="bg-[rgba(51,103,41,0.18)] w-full border-t-[4px] border-x-[4px] border-b-[4px] border-[#336729] 
+                rounded-[29.167px] py-2 shadow-lg text-green-700">
+
                     <p className="px-4 py-2 cursor-pointer">{content}</p>
                 </div>
             )}
