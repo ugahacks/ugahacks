1e9366edb5b1d8f29796f572c432429c5efba7eb
Ethan Ogle <encogle@gmail.com>
2024-10-24 13:48:50 -0400

    made footer

--- web/makeathon-3/app/page.tsx
+++ web/makeathon-3/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Toplandingpage from "./components/Toplandingpage/Toplandingpage";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className="bg-[#C4DB94] min-h-screen">
       <Toplandingpage />
+      <Footer />
     </div>
   );
 }
