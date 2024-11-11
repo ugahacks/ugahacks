8c21af452701127038fd120f0cc76c64399bcaaf
Vansh Arora <vansh94arora@gmail.com>
2024-11-11 15:09:53 -0500

    Done

--- web/makeathon-3/app/components/FAQ/Faq.tsx
+++ web/makeathon-3/app/components/FAQ/Faq.tsx
@@ -14,13 +14,9 @@ const Faq = () => {
                 alt="FAQ Title"
             />
             {/* Insert multiple dropdowns here with different content */}
-            <Dropdown title="Where will the makeathon be held?" content="IDK" />
-            <Dropdown title="How much will the event cost?" content="IDK" />
-            <Dropdown title="Can I start working on my project before the event?" content="IDK" />
-            <Dropdown title="What if I don't have a team or idea?" content="IDK" />
-            <Dropdown title="What are the rules all attendees must abide by?" content="IDK" />
-            <Dropdown title="How many people can I have on my team?" content="IDK" />
-            <Dropdown title="Who do I reach out to if I have more questions?" content="IDK" />
+            <Dropdown title="How much will the event cost?" content="Free, just make sure to RSVP through the form: (https://ugahacks.us2.list-manage.com/track/click?u=69a3e83ed9e60b403160ee5a8&id=6b647fa4bb&e=83411ef1f9)" />
+            <Dropdown title="Who do I reach out to if I have more questions?" content="You can reach out to the UGAHacks Team at hello@ugahacks.com!" />
+            <Dropdown title="Can I use Make Week workshops to satisfy UGAHacks EL Credit requirements?" content="At this time, Make Week workshops are not a method that counts towards UGAHacks EL Credit requirements." />
         </section>
     );
 };
