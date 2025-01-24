6e47b2b1f01ccd9f88f191305e802fcc6e338b6d
TheRyanMajd <rm97798@uga.edu>
2025-01-24 01:29:43 -0500

    ok fr this time xD

--- web/ugahacks-x/app/splash/Splash.tsx
+++ web/ugahacks-x/app/splash/Splash.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { React, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../styles/Splash.module.css";
 import FAQ from "../FAQ";
 
