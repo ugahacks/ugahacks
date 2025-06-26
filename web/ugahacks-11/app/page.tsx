e527963be6dfeb8bc76afea5098420d464613c08
TheRyanMajd <rm97798@uga.edu>
2025-06-26 10:14:27 -0400

    added date of event

--- web/ugahacks-11/app/page.tsx
+++ web/ugahacks-11/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       </div>
 
       <footer className="mt-4 text-center text-sm text-purple-300">
-        TBD · Athens, GA · Free to Attend · Food, Swag & Prizes
+        February 6th - 8th · Athens, GA · Free to Attend · Food, Swag & Prizes
       </footer>
     </main>
   );
