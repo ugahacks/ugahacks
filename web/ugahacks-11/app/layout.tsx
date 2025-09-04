1e6330825aa2cd4c36a8346725a60b912f183ef4
al69114 <167802218+al69114@users.noreply.github.com>
2025-09-04 16:03:02 -0400

    Navbar information updated

--- web/ugahacks-11/app/layout.tsx
+++ web/ugahacks-11/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
