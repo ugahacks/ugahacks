3b16e4ba6c54646205ed0d097d28cec98fd5bbc5
TheRyanMajd <rm97798@uga.edu>
2025-09-09 12:03:39 -0400

    Added Custom Cursor wand to website

--- web/ugahacks-11/app/layout.tsx
+++ web/ugahacks-11/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import CustomCursor from './components/CustomCursor';
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
-      </body>
+        <CustomCursor />{children}</body>
     </html>
   );
 }
