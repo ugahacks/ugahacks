4a9bf58b2699b0dcea33ba898fa2a26aff74556a
TheRyanMajd <rm97798@uga.edu>
2025-09-24 16:04:12 -0400

    funny comment mistake

--- web/ugahacks-11/app/page.tsx
+++ web/ugahacks-11/app/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import About from "./components/About/About";
 import Hero from "./components/Hero";
-import Schedule from "./components/Schedule";
 import OurTeam from "./components/OurTeam/OurTeam";
+import Schedule from "./components/Schedule";
 import SponsorTracks from "./components/SponsorTracks/SponsorTracks";
 import Tracks from "./components/Tracks/Tracks";
 
@@ -14,9 +14,9 @@ export default function Home() {
       <Tracks />
       <SponsorTracks />
       <Schedule />
-<!--    <FAQ goes here/>    -->
+      {/* <!--    <FAQ goes here/>    --> */}
       <OurTeam />
-<!-- <Sponsors Goes here/>  -->
+      {/* <!-- <Sponsors Goes here/>  --> */}
       <section className="h-screen bg-gray-100 flex items-center justify-center">
         <div className="text-center">
           <h2 className="text-4xl font-bold mb-4">More Content Below</h2>
@@ -25,7 +25,7 @@ export default function Home() {
           </p>
         </div>
       </section>
-      
+
     </main>
   );
 }
