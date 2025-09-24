4632b0fdb75c4d4e25aef979eee33374ff73915e
al69114 <167802218+al69114@users.noreply.github.com>
2025-09-24 17:17:22 -0400

    fixed lint errors

--- web/ugahacks-11/app/components/Sponsors/SponsorCard.tsx
+++ web/ugahacks-11/app/components/Sponsors/SponsorCard.tsx
@@ -6,9 +6,17 @@ interface SponsorCardProps {
 
 export default function SponsorCard({ imageUrl, href }: SponsorCardProps) {
  return (
-   <div
-     className="block w-full h-16 md:h-36 bg-white rounded-2xl md:rounded-3xl hover:scale-105 transition-transform duration-200"
+   <a
+     href={href}
+     target="_blank"
+     rel="noopener noreferrer"
+     className="block w-full h-16 md:h-36 bg-white rounded-2xl md:rounded-3xl hover:scale-105 transition-transform duration-200 overflow-hidden"
    >
-   </div>
+     <img
+       src={imageUrl}
+       alt="Sponsor logo"
+       className="w-full h-full object-contain p-2 md:p-4"
+     />
+   </a>
  );
 }
