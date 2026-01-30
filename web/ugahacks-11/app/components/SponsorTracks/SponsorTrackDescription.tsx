f5853e9c52511b7b0b4c7765c6e7e94cb3a539a0
al69114 <167802218+al69114@users.noreply.github.com>
2026-01-29 21:18:16 -0500

    Text issue fixed

--- web/ugahacks-11/app/components/SponsorTracks/SponsorTrackDescription.tsx
+++ web/ugahacks-11/app/components/SponsorTracks/SponsorTrackDescription.tsx
@@ -8,7 +8,7 @@ const SponsorTrackDescription: React.FC<SponsorTrackDescriptionProps> = ({
   text,
 }) => {
   return (
-    <div className="bg-[#D9D9D9] rounded-3xl shadow drop-shadow-lg p-2 max-w-3xl mx-auto font-encode-sans mt-6 py-6 min-h-[12rem] h-auto flex items-center justify-center">
+    <div className="bg-[#D9D9D9] rounded-3xl shadow drop-shadow-lg p-2 max-w-3xl mx-auto font-encode-sans mt-6 py-6 min-h-[12rem] h-auto flex items-center justify-center text-black">
         {text}
     </div>
   );
