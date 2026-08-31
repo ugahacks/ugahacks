a25d75ff3512c9fbe960708d8327dc4fa59ca61b
Mohan I <139013339+Moh4nIR@users.noreply.github.com>
2026-08-31 11:12:43 -0400

    what is uga hacks section 2 sections

--- web/ugahacks-12/src/app/page.tsx
+++ web/ugahacks-12/src/app/page.tsx
@@ -1,8 +1,10 @@
 import About from "~/components/About/About";
+import WhatIsUGAHacks from "~/components/WhatIsUGAHacks/WhatIsUGAHacks";
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col py-16 md:py-24">
+      <WhatIsUGAHacks />
       <About />
     </main>
   );
