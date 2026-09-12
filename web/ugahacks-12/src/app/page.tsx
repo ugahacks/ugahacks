e35d2baa8ab037215537a5d7724f2b836e451996
cameornh <crhalaby652@gmail.com>
2026-09-12 01:51:45 -0400

    Remove unrelated FAQ changes

--- web/ugahacks-12/src/app/page.tsx
+++ web/ugahacks-12/src/app/page.tsx
@@ -7,17 +7,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-1 flex-col">
-        <section
-          id="top"
-          className="flex flex-1 flex-col items-center justify-center gap-4 p-8"
-        >
-          <h1 className="text-4xl font-bold">{SITE_NAME}</h1>
-          <p className="text-lg">Coming soon.</p>
-        </section>
-        <Schedule />
-        <FAQ />
+      <main
+        id="top"
+        className="flex flex-1 flex-col items-center justify-center gap-4 p-8"
+      >
+        <h1 className="text-4xl font-bold">{SITE_NAME}</h1>
+        <p className="text-lg">Coming soon.</p>
       </main>
+      <Schedule />
+      <FAQ />
     </>
   );
 }
