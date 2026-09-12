fe93dd2643b5efb1e5514d3b9856ae233e3f3e44
coolhacker <shrutibmishra1@gmail.com>
2026-09-11 23:53:02 -0400

    sponsors section page

--- web/ugahacks-12/src/app/page.tsx
+++ web/ugahacks-12/src/app/page.tsx
@@ -1,10 +1,13 @@
 import { SITE_NAME } from "~/lib/site";
+import SponsorSection from "~/components/SponsorSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center gap-4 p-8">
+    <main className="flex flex-1 flex-col items-center justify-center gap-4 p-8 w-full">
       <h1 className="text-4xl font-bold">{SITE_NAME}</h1>
       <p className="text-lg">Coming soon.</p>
+      
+      <SponsorSection />
     </main>
   );
 }
